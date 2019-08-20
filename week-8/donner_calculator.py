d6582581d3fb9c77280aa1835c87418e53a55985
Adam Donner <adonner@my365.bellevue.edu>
2019-08-20 13:49:04 -0500

    Week 8 uploads

---
+++ week-8/donner_calculator.py
@@ -0,0 +1,22 @@
+""" 
+============================================
+; Title: donner_calculator.py
+; Author: Adam Donner
+; Date: 19 August 2019
+; Description:  A simple Python calculator
+;===========================================
+"""
+
+
+def add (number01, number02):
+    return number01 + number02
+
+def subtract (number01, number02):
+    return number01 - number02
+
+def divide (number01, number02):
+    return number01 / number02
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
---
+++ week-8/donner_hello_world.py
@@ -0,0 +1,10 @@
+""" 
+============================================
+; Title: donner_hello_world.py
+; Author: Adam Donner
+; Date: 19 August 2019
+; Description:  Prints Hello World using Python
+;===========================================
+"""
+
+print('Hello World')
